a3cdd4a291ce3d8ac39503441e596b28406c5bac
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-11-18 21:50:14 -0300

    tratando erros do servidor

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const debug = require('debug');
 const express = require('express');
+const { Console } = require('console');
 
 const app = express();
 const port = normalizePort(process.env.PORT || '3000');
@@ -20,6 +21,8 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
+
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val){
@@ -31,5 +34,26 @@ function normalizePort(val){
 
     if (port >= 0){
         return port;
+    }
+}
+
+function onError(error){
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated permission');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 }
