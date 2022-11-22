bad194fe91f5cea5f3a9a35504d04710f12e674a
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-11-21 23:20:09 -0300

    criando controller

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -10,18 +11,6 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put)
+router.delete('/', controller.delete);
